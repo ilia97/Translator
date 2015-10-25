11f86b14ad5bfebda24cea511a61dc648ecf8b7b
Ivan Hladush <gladush97@gmail.com>
2015-10-25 22:16:42 +0300

    Odna

--- Translator/Controllers/UsualController.cs
+++ Translator/Controllers/UsualController.cs
@@ -17,5 +17,36 @@ namespace Translator.Controllers
         {
             return View(db.Translations.ToList());
         }
+        [HttpPost]
+        public ActionResult GetWord(string Lab, string text) {
+            text = text.ToLower();
+            LanguageWord l = new LanguageWord();
+            if (text == null)
+            {
+                l.Id = 123;
+                l.Word = "";
+            }
+            else if (Lab == "Английский")
+            {
+                if (db.EnglishWords.Where(x => x.Word == text).Count() != 0)
+                {
+                    l.Word = db.EnglishWords.Where(x => x.Word == text).First().RussianTranslations.First().RussianWord.Word;
+                }
+                else {
+                    l.Word = text;
+                }
+            }
+            else {
+                if (db.RussianWords.Where(x => x.Word == text).Count() != 0)
+                {
+                    l.Word = db.RussianWords.Where(x => x.Word == text).First().EnglishTranslations.First().EnglishWord.Word;
+                }
+                else
+                {
+                    l.Word = text;
+                }
+            }
+            return PartialView(l);
+        }
     }
 }
