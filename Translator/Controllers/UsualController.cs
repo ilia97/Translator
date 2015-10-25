dc3242cebd62dccc39833fe9dbb55393be4a8cbc
Ivan Hladush <gladush97@gmail.com>
2015-10-25 17:46:32 +0300

    Add bd

---
+++ Translator/Controllers/UsualController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Translator.Models;
+using Translator.Models.DAL;
+
+namespace Translator.Controllers
+{
+ 
+    public class UsualController : Controller
+    {
+        TranslatorContext db = new TranslatorContext();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(db.Translations.ToList());
+        }
+    }
+}
--- Translator/Models/DAL/DbInitializer.cs
+++ Translator/Models/DAL/DbInitializer.cs
@@ -10,14 +10,28 @@ namespace Translator.Models.DAL
     {
         protected override void Seed(TranslatorContext db)
         {
-            EnglishWord ew1 = new EnglishWord { Id = 1, Word = "elephant", Transcription = "[ˈɛlɪf(ə)nt]" };
-            db.EnglishWords.Add(ew1);
 
-            RussianWord rw1 = new RussianWord { Id = 1, Word = "слон", Transcription = "[сло́н]" };
-            db.RussianWords.Add(rw1);
+            db.EnglishWords.Add(new EnglishWord { Id = 1, Word = "elephant", Transcription = "[ˈɛlɪf(ə)nt]" });
+            db.EnglishWords.Add(new EnglishWord { Id = 2, Word = "apple", Transcription = "[ ˈæp.l̩ ]" });
+            db.EnglishWords.Add(new EnglishWord { Id = 3, Word = "banana", Transcription = "[ bəˈnɑː.nə ]" });
+            db.EnglishWords.Add(new EnglishWord { Id = 4, Word = "bamboo", Transcription = "[ bæmˈbuː ]" });
+            db.EnglishWords.Add(new EnglishWord { Id = 5, Word = "bomb", Transcription = "[ bɒm ]" });
+            db.EnglishWords.Add(new EnglishWord { Id = 6, Word = "cream", Transcription = "[ kriːm ]" });
 
-            Translation tr1 = new Translation { EnglishWordId = 1, RussianWordId = 1, Picture = "~/Pictures/elephant.jpg" };
-            db.Translations.Add(tr1);
+            
+            db.RussianWords.Add(new RussianWord { Id = 1, Word = "слон", Transcription = "[сло́н]" });
+            db.RussianWords.Add(new RussianWord { Id = 2, Word = "яблоко", Transcription = "[я́блоко]" });
+            db.RussianWords.Add(new RussianWord { Id = 3, Word = "банан", Transcription = "[бана́н]" });
+            db.RussianWords.Add(new RussianWord { Id = 4, Word = "бамбук", Transcription = "[бʌмбу́к]" });
+            db.RussianWords.Add(new RussianWord { Id = 5, Word = "бомба", Transcription = "[бо́мба]" });
+            db.RussianWords.Add(new RussianWord { Id = 6, Word = "крем", Transcription = "[кр'е́м]" });
+
+            db.Translations.Add(new Translation { EnglishWordId = 1, RussianWordId = 1, Picture = "~/Pictures/elephant.jpg" });
+            db.Translations.Add(new Translation { EnglishWordId = 2, RussianWordId = 2, Picture = "~/Pictures/apple.jpg" });
+            db.Translations.Add(new Translation { EnglishWordId = 3, RussianWordId = 3, Picture = "~/Pictures/banana.jpg" });
+            db.Translations.Add(new Translation { EnglishWordId = 4, RussianWordId = 4, Picture = "~/Pictures/bamboo.jpg" });
+            db.Translations.Add(new Translation { EnglishWordId = 5, RussianWordId = 5, Picture = "~/Pictures/bomb.jpg" });
+            db.Translations.Add(new Translation { EnglishWordId = 6, RussianWordId = 6, Picture = "~/Pictures/cream.jpg" });
 
             base.Seed(db);
         }
